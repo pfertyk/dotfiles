005b662c51689819a1d8cfd4bb3b69b9d159d678
Paweł Fertyk <pawel.fertyk@gmail.com>
2016-02-22 21:59:34 +0100

    Add terminal config (cursor shape)

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim profile git-prompt.sh local/share/xfce4"    # list of files/folders to symlink in homedir
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="bashrc vimrc vim profile git-prompt.sh local/share/xfce4 config/xfce4/terminal"
 
 echo "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir $olddir
