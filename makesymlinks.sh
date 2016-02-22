13ad524c12d5425d1fa0ae9f3409354dc86382d0
Paweł Fertyk <pawel.fertyk@gmail.com>
2016-02-22 21:30:21 +0100

    Run Vim plugins installation

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -4,28 +4,24 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-########## Variables
-
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim profile git-prompt.sh"    # list of files/folders to symlink in homedir
 
-##########
+echo "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir $olddir
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
+echo "Changing to the $dir directory ..."
 cd $dir
-echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+echo "Installing Vundle and other Vim plugins ..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
