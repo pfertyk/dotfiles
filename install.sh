0ed9e7d690a1e33f4c5a5af8da13a11cd3260015
Paweł Fertyk <pawel.fertyk@gmail.com>
2016-02-24 20:41:07 +0100

    Add quicktile config file

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="bashrc vimrc vim profile git-prompt.sh local/share/xfce4 config/xfce4/terminal"
+files="bashrc vimrc vim profile git-prompt.sh local/share/xfce4 config/xfce4/terminal config/quicktile.cfg"
 
 echo "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir $olddir
