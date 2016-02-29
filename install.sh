b4f6b38001c80ff19216e8ddb465d6f7f25ec551
Paweł Fertyk <pawel.fertyk@gmail.com>
2016-02-29 13:21:04 +0100

    Add date to backup dir and edit install messages

--- install.sh
+++ install.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-dir=~/dotfiles
-olddir=~/dotfiles_old
+dir=$PWD
+bck_dir="dotfiles_bck_$(date +%Y_%m_%d)"
 files="gitconfig bashrc vimrc vim profile git-prompt.sh 
 local/share/xfce4 config/xfce4/terminal config/quicktile.cfg"
 
-echo "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir $olddir
-
-echo "Changing to the $dir directory ..."
-cd $dir
+echo "Creating $bck_dir for backup of any existing dotfiles"
+rm -rf ~/$bck_dir
+mkdir ~/$bck_dir
 
 for file in $files; do
-    echo "Creating symlink to $file in home directory."
-    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file"
+    mv ~/.$file ~/$bck_dir/
     ln -s $dir/$file ~/.$file
 done
 
-echo "Installing Vundle and other Vim plugins ..."
+echo "Installing Vundle and other Vim plugins"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
