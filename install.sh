070004f0796d5a0099c2592e55b65b428dac79ba
Paweł Fertyk <pawel.fertyk@gmail.com>
2016-03-17 21:52:15 +0100

    Ignore thunar.xml, refactor install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 dir=$PWD
 bck_dir="dotfiles_bck_$(date +%Y_%m_%d)"
-files="gitconfig bashrc vimrc vim profile git-prompt.sh inputrc
-local/share/xfce4 config/xfce4/terminal config/quicktile.cfg
-config/xfce4/xfconf"
+files="
+bashrc
+gitconfig
+git-prompt.sh
+inputrc
+profile
+vim
+vimrc
+config/quicktile.cfg
+config/xfce4/terminal
+config/xfce4/xfconf
+local/share/xfce4
+"
 
 echo "Creating $bck_dir for backup of any existing dotfiles"
 rm -rf ~/$bck_dir
