6bb97de32c9f901680ca5c2ea65c22d01dec2aef
Paweł Fertyk <pfertyk@pfertyk.me>
2017-11-12 20:51:00 +0100

    Use window class in terminal script, add readme

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
+# A simple script that turns normal terminal into a drop-down terminal.
+#
+# I wrote it because XFCE terminal in drop-down mode was not detected as a
+# normal window,  making it impossible to go back to it by cycling through
+# the list of windows (Alt+Tab).
+#
+# Behavior:
+# - if there is no terminal open yet, open it in fullscreen mode
+# - if the terminal is open but not active, activate it
+# - if the terminal is open and active, minimize it
+#
+# Requirements:
+# - xdotool
+# - wmctrl
+# - exo-open (exo-utils)
+
 function get_terminal_id()
 {
 	local terminal_id=-1
-	while read process_id desktop_id machine process_name
+	while read process_id desktop_id class rest
 	do
-		# TODO: use window class instead of process name
-		if [ "$desktop_id" -eq "$current_desktop" ] && [[ "$process_name" == *"@$machine: "* ]]
+		if [ "$desktop_id" -eq "$1" ] && [[ "$class" == *"terminal"* ]]
 		then
 			terminal_id=$process_id
 		fi
-	done <<< "$(wmctrl -l)"
+	done <<< "$(wmctrl -lx)"
 	echo $terminal_id
 }
 
@@ -23,15 +38,14 @@ do
 	fi
 done <<< "$(wmctrl -d)"
 
-
-terminal_id=`get_terminal_id`
+terminal_id=`get_terminal_id $current_desktop`
 
 if [ $terminal_id == -1 ]
 then
 	exo-open --launch TerminalEmulator
 	while [ $terminal_id == -1 ]
 	do
-		terminal_id=`get_terminal_id`
+		terminal_id=`get_terminal_id $current_desktop`
 	done
 	wmctrl -ir $terminal_id -b add,fullscreen
 else
